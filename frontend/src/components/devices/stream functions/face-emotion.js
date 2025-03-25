e72fc80e0a0c237a1a69fe5ca41b56173993224a
Esteban Romero <esromerog@10-18-123-16.dynapool.wireless.nyu.edu>
2025-03-25 15:09:29 -0400

    Added Face Emotion module

--- frontend/src/components/devices/stream functions/face-emotion.js
+++ frontend/src/components/devices/stream functions/face-emotion.js
@@ -1,79 +1,60 @@
-import { FaceEmotionRecognition } from "../../../utility/face_emotion";
-import { render, createPortal, createRoot } from "react-dom";
-import { useEffect, useMemo, useRef } from "react";
+import store from "../../../store/store";
 
-export function connectVideoEmotion() {
+export function connectVideoEmotion(changeConnectionStatus) {
   const newWindow = window.open(
-    "about:blank",
+    "/audiovideo/videoEmotion/index.html",
     "_blank",
     "popup"
   );
 
-  newWindow.document.title = "Video Emotion";
-  const container = document.createElement("div");
-  container.id = "root";
-  newWindow.document.body.appendChild(container);
+  const id = "Face Emotion";
+
+  store.dispatch({
+    type: "devices/create",
+    payload: {
+      id: id,
+      metadata: {
+        device: "Video Emotion",
+        type: "default",
+        id: id,
+        connected: true,
+      },
+    },
+  });
+
+  newWindow.onbeforeunload = () => {
+    changeConnectionStatus("disconnected");
+    store.dispatch({
+      type: "devices/updateMetadata",
+      payload: {
+        id: id,
+        field: "connected",
+        data: false,
+      },
+    });
+  };
+
+  changeConnectionStatus("awaiting");
+  const bc = new BroadcastChannel("face-emotion");
+
+  bc.onmessage = (eventMessage) => {
+    const rawData = JSON.parse(eventMessage.data);
+    const expressionData = rawData?.expressions[0]?.expressions;
+    store.dispatch({
+      type: "devices/streamUpdate",
+      payload: {
+        id: id,
+        data: expressionData,
+      },
+    });
+  };
+
+  changeConnectionStatus("connected");
+
 
   window.onbeforeunload = () => {
     if (newWindow && !newWindow.closed) {
       newWindow.close();
     }
   };
-
-  return render(<VideoWindow />, container);
-}
-
-
-function VideoWindow({ onClose = () => {} }) {
-  // const [videoInitialized, setVideoInitialized] = useState(false);
-  const videoRef = useRef(null);
-  const canvasRef = useRef(null);
-
-  async function startPredictions() {
-    const modelPaths = "/audiovideo/videoEmotion/models";
-
-    const faceEmotion = new FaceEmotionRecognition(
-      modelPaths,
-      videoRef.current,
-      canvasRef.current
-    );
-    await faceEmotion.connect();
-    await faceEmotion.stream();
-  }
-
-  useEffect(() => {
-
-    if (navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then((stream) => {
-          console.log(videoRef.current);
-          videoRef.current.srcObject = stream;
-          startPredictions();
-        })
-        .catch((err) => {
-          console.log("Something went wrong!");
-        });
-    }
-
-    window.onbeforeunload = () => {
-      onClose();
-    };
-  });
-
-  // https://github.com/samdutton/simpl/blob/gh-pages/getusermedia/sources/js/main.js
-  // 
-
-  return (
-    <div className="w-100 h-100">
-      <video
-        id="video"
-        autoPlay={true}
-        muted
-        playsInline
-        ref={videoRef}
-      ></video>
-      <canvas id="canvas" ref={canvasRef}></canvas>
-    </div>
-  );
 }
--- frontend/src/components/devices/stream functions/face_sync.js
+++ frontend/src/components/devices/stream functions/face_sync.js
@@ -1,21 +1,21 @@
-import FaceSync from '../../../utility/faceSync';
-import store from '../../../store/store';
+import FaceSync from "../../../utility/faceSync";
+import store from "../../../store/store";
 
 export function connectFaceSync(changeConnectionStatus) {
-    window.open("/audiovideo/facesync/index.html", "_blank", "popup");
-    const bc = new BroadcastChannel('face-sync');
-    const face = new FaceSync(bc);
-    face.initialize();
+  window.open("/audiovideo/facesync/index.html", "_blank", "popup");
+  const bc = new BroadcastChannel("face-sync");
+  const face = new FaceSync(bc);
+  face.initialize();
 
-    changeConnectionStatus("awaiting");
-    setTimeout(() => {
-        const id = "Face Synchrony";
-        if (face.isOpen()) {
-            changeConnectionStatus("connected");
-            store.dispatch({
-                type: "devices/create",
-                payload: {
-                id: id,
+  changeConnectionStatus("awaiting");
+  setTimeout(() => {
+    const id = "Face Synchrony";
+    if (face.isOpen()) {
+      changeConnectionStatus("connected");
+      store.dispatch({
+        type: "devices/create",
+        payload: {
+          id: id,
           metadata: {
             device: "FaceSync",
             type: "default",
